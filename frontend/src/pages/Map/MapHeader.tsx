2e27338d93f844e10f4de1698788b2154abe55ee
frooia <frooia77@gmail.com>
2022-11-07 19:53:26 -0500

    Small ESLint changes done

--- frontend/src/pages/Map/MapHeader.tsx
+++ frontend/src/pages/Map/MapHeader.tsx
@@ -2,10 +2,6 @@ import React from "react";
 import "./MapHeader.css";
 
 class MapHeader extends React.Component<{}, {}> {
-  constructor(props: any) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="Map">
