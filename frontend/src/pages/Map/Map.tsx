c09ab1abb0621eb1651bd3c678e696cbffba2aea
Liam Rosenfeld <me@liamrosenfeld.com>
2022-12-06 18:12:26 -0500

    Link Reporting to Backend

--- frontend/src/pages/Map/Map.tsx
+++ frontend/src/pages/Map/Map.tsx
@@ -2,7 +2,7 @@
   Actual implementation of the map
   Markers are grabbed from the backend and rendered onto a google maps API
 */
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import { mapContainerStyle, center, zoom, options } from './settings';
 import ReportMarker, { ReportMarkerType } from './Markers';
@@ -10,8 +10,7 @@ import { createStyles, LoadingOverlay, Slider, Stack } from '@mantine/core';
 import ReportsButton from "../Reports/ReportsButton";
 
 // stylesheet attempt for removing scroll bar overflow
-const useStyles = createStyles( ( theme ) => ( {
-
+const useStyles = createStyles((theme) => ({
   wrapper: {
     display: 'flex',
     maxWidth: '100%',
@@ -27,59 +26,51 @@ const useStyles = createStyles( ( theme ) => ( {
     position: 'absolute',
     bottom: '80px',
   },
-} ) );
+}));
 
 // render the map if the API key is available
-const Map: React.FC = () =>
-{
+function Map() {
   // get api key
-  const [ key, setKey ] = useState<string>();
-  useEffect( () =>
-  {
-    async function getKey ()
-    {
-      const response = await fetch( '/api/keys/map' );
+  const [key, setKey] = useState<string>();
+  useEffect(() => {
+    async function getKey() {
+      const response = await fetch('/api/keys/map');
       const key = await response.text();
-      setKey( key );
+      setKey(key);
     }
 
     getKey();
-  }, [] );
+  }, []);
 
   // don't load the map until we have obtained the API key
-  return ( key !== undefined ) && key !== "" ? <div style={ { margin: 0 } }><MapChild apiKey={ key } /></div> :
+  return (key !== undefined) && key !== "" ? <div style={{ margin: 0 }}><MapChild apiKey={key} /></div> :
     <LoadingOverlay
-      loaderProps={ { size: 'xl', color: 'black', variant: 'bars' } }
-      overlayOpacity={ 0 }
+      loaderProps={{ size: 'xl', color: 'black', variant: 'bars' }}
+      overlayOpacity={0}
       overlayColor="#c5c5c5"
       visible
     />;
 };
 
 
-const MapChild: React.FC<{ apiKey: string; }> = ( props ) =>
-{
-  const sliderRef = useRef<HTMLDivElement>( null );
+function MapChild(props: { apiKey: string; }) {
+  const sliderRef = useRef<HTMLDivElement>(null);
   var hoursBack = 11;
   // fetch data from the api
-  const fetchData = () =>
-  {
-    fetch( "/api/reports/recent?hours_back=" + hoursBack )
-      .then( ( response ) => response.json() )
-      .then( ( response ) =>
-      {
-        setRawPoints( response );
-      } )
-      .catch( () =>
-      {
-        console.log( 'error' );
-      } );
+  const fetchData = () => {
+    fetch("/api/reports/recent?hours_back=" + hoursBack)
+      .then((response) => response.json())
+      .then((response) => {
+        setRawPoints(response);
+      })
+      .catch(() => {
+        console.log('error');
+      });
   };
 
   // update the hours back and resend api
-  const onUpdate = () =>
-  {
-    hoursBack = Number( sliderRef.current?.lastElementChild?.attributes[ 1 ].value );
+  const onUpdate = () => {
+    hoursBack = Number(sliderRef.current?.lastElementChild?.attributes[1].value);
     fetchData();
   };
 
@@ -89,31 +80,29 @@ const MapChild: React.FC<{ apiKey: string; }> = ( props ) =>
   // var baseLng = -82.360;
 
   // attempt to grab data from API and store in rawPoints
-  const [ rawPoints, setRawPoints ] = useState( [] );
-  const [ points, setPoints ] = useState<Array<ReportMarkerType>>();
+  const [rawPoints, setRawPoints] = useState([]);
+  const [points, setPoints] = useState<Array<ReportMarkerType>>();
   const { classes } = useStyles();
 
 
   // waits until rawPoints is fully set before converting the generic rawPoints to Strictly type Points
-  useEffect( () =>
-  {
+  useEffect(() => {
     var tmpMarkers: Array<ReportMarkerType> = new Array<ReportMarkerType>();
 
-    for ( var i = 0; i < rawPoints.length; i++ )
-    {
+    for (var i = 0; i < rawPoints.length; i++) {
       var tmpLatLng: google.maps.LatLngLiteral = {
-        lat: rawPoints[ i ][ 'loc_x' ],
-        lng: rawPoints[ i ][ 'loc_y' ],
+        lat: rawPoints[i]['loc_x'],
+        lng: rawPoints[i]['loc_y'],
       };
       var tmpReportMarker: ReportMarkerType = {
-        id: rawPoints[ i ][ 'id' ],
+        id: rawPoints[i]['id'],
         location: tmpLatLng,
       };
-      tmpMarkers.push( tmpReportMarker );
+      tmpMarkers.push(tmpReportMarker);
     }
 
-    setPoints( tmpMarkers );
-  }, [ rawPoints ] );
+    setPoints(tmpMarkers);
+  }, [rawPoints]);
 
   /******************************************/
 
@@ -122,16 +111,14 @@ const MapChild: React.FC<{ apiKey: string; }> = ( props ) =>
 
   // load the map
   const { isLoaded, loadError } = useJsApiLoader
-    ( {
+    ({
       id: 'google-map-ide',
       googleMapsApiKey: props.apiKey
-    } );
+    });
 
-  const renderMap = () =>
-  {
-    const onLoad = ( _map: google.maps.Map ): void =>
-    {
-      console.log( "map loaded" );
+  const renderMap = () => {
+    const onLoad = (_map: google.maps.Map): void => {
+      console.log("map loaded");
       fetchData();
     };
 
@@ -143,50 +130,50 @@ const MapChild: React.FC<{ apiKey: string; }> = ( props ) =>
     ];
 
     return (
-      <Stack className={ classes.wrapper }>
+      <Stack className={classes.wrapper}>
         <GoogleMap
-          mapContainerStyle={ mapContainerStyle }
-          options={ options as google.maps.MapOptions }
-          center={ center }
-          zoom={ zoom }
-          onLoad={ onLoad }
+          mapContainerStyle={mapContainerStyle}
+          options={options as google.maps.MapOptions}
+          center={center}
+          zoom={zoom}
+          onLoad={onLoad}
         >
           {
-            points?.map( point =>
+            points?.map(point =>
             (
-              <ReportMarker ReportMarkerStruct={ point } key={ point.id } />
-            ) )
+              <ReportMarker ReportMarkerStruct={point} key={point.id} />
+            ))
 
           }
-          <ReportsButton />
+          <ReportsButton reloadCats={fetchData} />
         </GoogleMap>
-        <Slider className={ classes.slider }
+        <Slider className={classes.slider}
           color="blue"
-          defaultValue={ 11 }
-          marks={ marks }
+          defaultValue={11}
+          marks={marks}
           labelTransition="fade"
-          size={ 4 }
-          ref={ sliderRef }
-          onChangeEnd={ onUpdate }
-          styles={ ( theme ) => ( {
+          size={4}
+          ref={sliderRef}
+          onChangeEnd={onUpdate}
+          styles={(theme) => ({
             track: {
               boxShadow: '5px 5px 7px black',
               backgroundColor:
-                theme.colorScheme === 'dark' ? theme.colors.dark[ 3 ] : theme.colors.red,
+                theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.red,
             },
             mark: {
               width: 10,
               height: 10,
               borderRadius: 6,
               transform: 'translateX(-3px) translateY(-2px)',
-              borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[ 3 ] : theme.colors.blue[ 1 ],
+              borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.blue[1],
             },
             markFilled: {
-              borderColor: theme.colors.blue[ 6 ],
+              borderColor: theme.colors.blue[6],
             },
             markLabel: {
               fontSize: theme.fontSizes.xs,
-              color: theme.colors.dark[ 4 ],
+              color: theme.colors.dark[4],
               marginBottom: 5,
               marginTop: 3,
             },
@@ -197,15 +184,14 @@ const MapChild: React.FC<{ apiKey: string; }> = ( props ) =>
               borderWidth: 1,
               boxShadow: theme.shadows.sm,
             },
-          } ) }
-          scale={ ( v ) => Math.round( ( v ** 2 ) / 5 ) }
+          })}
+          scale={(v) => Math.round((v ** 2) / 5)}
         />
       </Stack>
     );
   };
 
-  if ( loadError )
-  {
+  if (loadError) {
     return <div>Map cannot be loaded right now, sorry.</div>;
   }
 
--- frontend/src/pages/Reports/ReportsButton.tsx
+++ frontend/src/pages/Reports/ReportsButton.tsx
@@ -1,5 +1,4 @@
-import
-{
+import {
   Modal,
   Group,
   Text,
@@ -7,64 +6,106 @@ import
   Stack,
   Button,
   Paper,
+  Space,
+  Alert,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useState } from 'react';
 import { GoogleMap } from '@react-google-maps/api';
 import { mapContainerStyleMini, center, zoom, miniOptions } from '../Map/settings';
-import ReportMarker, { ReportMarkerType } from '../Map/Markers';
+import ReportMarker from '../Map/Markers';
+import { IconAlertCircle } from "@tabler/icons";
+
+interface ReportRequest {
+  catName: string,
+  notes: string,
+  lat: number | null,
+  lng: number | null,
+}
 
 
-function ReportForm ()
-{
-  const form = useForm( {
+function ReportForm(props: {
+  setOpened: (_: boolean) => void;
+  reloadCats: () => void;
+}) {
+  const form = useForm({
     initialValues: {
       catName: "",
       notes: "",
-    },
+      lat: null,
+      lng: null,
+    } as ReportRequest,
     validate: {
-      catName: ( value ) => ( value === "" ? null : "Invalid Cat Name" )
+      catName: (value) => (value === "" ? "Must name the cat" : null)
     },
-  } );
+  });
 
-  const [ pointClicked, setPointClicked ] = useState<Boolean>( false );
-  const [ point, setPoint ] = useState<ReportMarkerType>();
+  const [pointClicked, setPointClicked] = useState(false);
+  const [submitError, setSubmitError] = useState("");
 
-  const mapClicked = ( event: google.maps.MapMouseEvent ) =>
-  {
-    var tmpPoint: ReportMarkerType;
-
-    var tmpLatLng = {
-      lat: Number( event.latLng?.lat() ),
-      lng: Number( event.latLng?.lng() )
-    };
-    tmpPoint = {
-      id: 0,
-      location: tmpLatLng,
-    };
-    console.log( tmpPoint );
-    setPoint( tmpPoint );
-    setPointClicked( true );
+  const mapClicked = (event: google.maps.MapMouseEvent) => {
+    form.setFieldValue('lat', event.latLng?.lat() ?? null);
+    form.setFieldValue('lng', event.latLng?.lng() ?? null);
+    setPointClicked(true);
   };
+
+  async function submitReport(form: ReportRequest) {
+    let token = localStorage.getItem("token");
+    if (token == null) { return; }
+
+    if (form.lat == null || form.lng == null) {
+      setSubmitError("Must select location");
+      return;
+    }
+
+    let response = await fetch("/api/reports/new", {
+      method: "POST",
+      headers: new Headers({
+        "Authorization": "Bearer " + token,
+      }),
+      body: new URLSearchParams({
+        cat_name: form.catName,
+        notes: form.notes,
+        loc_x: form.lat.toString(),
+        loc_y: form.lng.toString(),
+      })
+    });
+
+    switch (response.status) {
+      case 200:
+        let id = await response.json();
+        console.log(id);
+        setSubmitError("");
+        props.reloadCats();
+        props.setOpened(false);
+        break;
+      case 401:
+        // jwt is not valid -> logout
+        localStorage.removeItem("token");
+        break;
+      default:
+        console.error("unexpected error getting myself: " + response.statusText);
+    }
+  }
 
   return (
     <Stack spacing="sm">
-      <form>
+      <form onSubmit={form.onSubmit(submitReport)}>
         <TextInput
           withAsterisk
           label="Cat Name"
           placeholder="Cat Name"
-          { ...form.getInputProps( "catName" ) }
+          {...form.getInputProps("catName")}
         />
 
         <TextInput
           label="Optional Notes"
           placeholder="Notes"
-          { ...form.getInputProps( "notes" ) }
+          {...form.getInputProps("notes")}
         />
 
         <Group position="apart">
-          <Text size="sm" pb={ 3 } pt={ 3 } pr={ 0 }>
+          <Text size="sm" pb={3} pt={3} pr={0}>
             Location
           </Text>
           <Text size="xs">
@@ -74,52 +115,73 @@ function ReportForm ()
 
         <Paper withBorder radius="sm" >
           <GoogleMap
-            mapContainerStyle={ mapContainerStyleMini }
-            options={ miniOptions as google.maps.MapOptions }
-            center={ center }
-            zoom={ zoom }
-            onClick={ mapClicked }
-          // onLoad={ onLoad }
+            mapContainerStyle={mapContainerStyleMini}
+            options={miniOptions as google.maps.MapOptions}
+            center={center}
+            zoom={zoom}
+            onClick={mapClicked}
           >
             {
-              pointClicked && ( <ReportMarker ReportMarkerStruct={ point! } key={ point!.id } /> )
+              pointClicked && (<ReportMarker ReportMarkerStruct={
+                {
+                  id: 0,
+                  location: {
+                    lat: form.values.lat!,
+                    lng: form.values.lng!
+                  }
+                }
+              } key={"current_loc"} />)
             }
           </GoogleMap>
         </Paper>
 
+        <Space h="md" />
 
-      </form>
-      <Group>
-        <Button variant="filled" color="indigo">
+        <Button variant="filled" color="indigo" type="submit">
           Submit
         </Button>
-      </Group>
+      </form>
+
+      {submitError !== "" &&
+        <>
+          <Space h="sm" />
+          <Alert
+            icon={<IconAlertCircle size={16} />}
+            color="orange"
+            radius="md"
+            withCloseButton onClose={() => setSubmitError("")}
+          >
+            {submitError}
+          </Alert>
+        </>
+      }
     </Stack>
   );
 }
 
-export function ReportsButton ()
-{
-  const [ opened, setOpened ] = useState( false );
+export function ReportsButton(props: { reloadCats: () => void; }) {
+  const [opened, setOpened] = useState(false);
+
+  if (localStorage.getItem("token") == null) {
+    return (<></>);
+  }
 
   return (
     <div>
       <Modal
-        opened={ opened }
-        onClose={ () => setOpened( false ) }
+        centered
+        opened={opened}
+        onClose={() => setOpened(false)}
         title="New Report"
       >
-
-        {/*<CatNameEditor />*/ }
-        <ReportForm />
-
+        <ReportForm setOpened={setOpened} reloadCats={props.reloadCats} />
       </Modal>
 
       <Button
         variant="filled"
         color="indigo"
-        onClick={ () => setOpened( true ) }
-        style={ { position: "absolute", zIndex: "100" } }
+        onClick={() => setOpened(true)}
+        style={{ position: "absolute", zIndex: "100" }}
       >
         New Report
       </Button>
